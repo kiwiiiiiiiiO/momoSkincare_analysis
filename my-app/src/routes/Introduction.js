6a040fa621ab28d8bd7cdc8505f63ef0940c9b71
歐杰妮 <jennifer97279@gmail.com>
2023-11-07 09:21:44 +0800

    update my-app

--- my-app/src/routes/Introduction.js
+++ my-app/src/routes/Introduction.js
@@ -1,10 +1,72 @@
 import React from 'react'
+import { Card, Col, Row } from 'antd';
+const RowStyle={
+  backgroundColor:'blue',
+  minWith:"100%",
+  marginTop:'10px'
+}
+const ColStyle={
 
+}
 const Introduction = () => {
   return (
-    <div>
-         <h1>Introduction</h1>
-    </div>
+    <Row style={RowStyle}>
+      <Col style={ColStyle} span={24}>
+        <Card title="Card title" bordered={false}>
+          Card content
+        </Card>
+      </Col>
+      <Col style={ColStyle} span={24}>
+        <Card title="Card title" bordered={false}>
+          Card content
+        </Card>
+      </Col>
+      <Col style={ColStyle} span={24}>
+        <Card title="Card title" bordered={false}>
+          Card content
+        </Card>
+      </Col>
+      <Col style={ColStyle} span={24}>
+        <Card title="Card title" bordered={false}>
+          Card content
+        </Card>
+      </Col>
+      <Col style={ColStyle} span={24}>
+        <Card title="Card title" bordered={false}>
+          Card content
+        </Card>
+      </Col>
+      <Col style={ColStyle} span={24}>
+        <Card title="Card title" bordered={false}>
+          Card content
+        </Card>
+      </Col>
+      <Col style={ColStyle} span={24}>
+        <Card title="Card title" bordered={false}>
+          Card content
+        </Card>
+      </Col>
+      <Col style={ColStyle} span={24}>
+        <Card title="Card title" bordered={false}>
+          Card content
+        </Card>
+      </Col>
+      <Col style={ColStyle} span={24}>
+        <Card title="Card title" bordered={false}>
+          Card content
+        </Card>
+      </Col>
+      <Col style={ColStyle} span={24}>
+        <Card title="Card title" bordered={false}>
+          Card content
+        </Card>
+      </Col>
+      <Col style={ColStyle} span={24}>
+        <Card title="Card title" bordered={false}>
+          Card content
+        </Card>
+      </Col>
+    </Row>
   )
 }
 
